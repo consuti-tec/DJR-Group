02e54a876e422fb601c9463f3d3ba358ef367398
consuti-tec <consuti@consuti.com.br>
2023-03-31 15:17:39 -0300

    Initial commit

---
+++ src/AppConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ColorTheme: "#283673",
+  UrlPortal: "https://plataforma.sporti.com.br/djr-group",
+  UrlPainel: "https://painel.sporti.com.br/",
+  TimeSplashScreen: 3000,
+};
